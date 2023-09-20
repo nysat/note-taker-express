79ae88631a9124009e3cbe8253d19d4b4c42321d
nysat <nysa-cere3@hotmail.com>
2023-09-20 01:52:52 -0400

    added the delete and connected the html pages

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const uuid =require('uuid');
+const express = require("express")
+const fs = require("fs");
+const notes = require("./db/db.json");
+const path = require("path");
+const uuid = require("uuid");
+const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
-const PORT = process.env.PORT || 4001;
+
 const app = express(); 
+const PORT = process.env.PORT || 4001;
 
+
+//middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
 //API ROUTES gets notes and saves it in db.json 
 app.get("/api/notes", (req, res) => {
@@ -20,4 +28,24 @@ app.post("/api/notes", (req, res) => {
     notes.push(newNotes);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
+})
+
+//delete 
+app.delete("/api/notes/:id", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const deleteNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync("./db/db.json", JSON.stringify(deleteNote));
+    res.json(deleteNote);
+});
+
+app.get("/", function (req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+
+});
+app.get("/notes", function (req, res){
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.listen(PORT, function(){
+    console.log('App is listening on port: ' + PORT)
 })
